47eacb1473d478ed920fbef2702b84eadfe30ff2
yangjinzhong <fredisstillalmighty@gmail.com>
2015-11-26 18:07:42 +0800

    Store

---
+++ FYUIScrollViewDemo/FYUIScrollViewDemo/Stores/FYStore.h
@@ -0,0 +1,13 @@
+//
+//  FYStore.h
+//  FYUIScrollViewDemo
+//
+//  Created by Frankenstein Yang on 11/26/15.
+//  Copyright © 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FYStore : NSObject
+
+@end
---
+++ FYUIScrollViewDemo/FYUIScrollViewDemo/Stores/FYStore.m
@@ -0,0 +1,13 @@
+//
+//  FYStore.m
+//  FYUIScrollViewDemo
+//
+//  Created by Frankenstein Yang on 11/26/15.
+//  Copyright © 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import "FYStore.h"
+
+@implementation FYStore
+
+@end
