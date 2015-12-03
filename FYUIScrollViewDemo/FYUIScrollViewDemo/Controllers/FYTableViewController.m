7d902c14a63b192dac7cd339a5de10d496475a44
yangjinzhong <fredisstillalmighty@gmail.com>
2015-12-03 19:45:13 +0800

    UITableView下拉刷新。

--- FYUIScrollViewDemo/FYUIScrollViewDemo/Controllers/FYTableViewController.m
+++ FYUIScrollViewDemo/FYUIScrollViewDemo/Controllers/FYTableViewController.m
@@ -8,15 +8,25 @@
 
 #import <libextobjc/EXTScope.h>
 #import <Masonry/Masonry.h>
+#import <MJRefresh/MJRefresh.h>
 
 #import "FYTableViewController.h"
 #import "FYUIButton.h"
 
 #define kSCREEN_SIZE ([UIScreen mainScreen].bounds.size)
 
+@interface FYTableViewController ()
+
+@property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) NSArray     *data;
+
+@end
+
 @implementation FYTableViewController
 
 - (void)viewDidLoad {
+    
+    [super viewDidLoad];
     FYUIButton *backBtn = [[FYUIButton alloc] initWithFrame:CGRectZero style:FYUIButtonStyleTranslucent];
     backBtn.text = @"返回首页";
     backBtn.font = [UIFont systemFontOfSize:14.0f];
@@ -26,8 +36,8 @@
     [backBtn addTarget:self action:@selector(goToHomepageBtnClicked) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:backBtn];
     
-    UITableView *tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 100, kSCREEN_SIZE.width, kSCREEN_SIZE.height - 100)];
-    [self.view addSubview:tableView];
+    _tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 100, kSCREEN_SIZE.width, kSCREEN_SIZE.height - 100)];
+    [self.view addSubview:_tableView];
     
     @weakify(self);
     [backBtn mas_makeConstraints:^(MASConstraintMaker *make) {
@@ -36,6 +46,11 @@
         make.centerX.mas_equalTo(self.view.mas_centerX);
         make.size.mas_equalTo(CGSizeMake(160, 40));
     }];
+    
+}
+
+- (NSArray *)data {
+    return _data;
 }
 
 - (void)goToHomepageBtnClicked {
