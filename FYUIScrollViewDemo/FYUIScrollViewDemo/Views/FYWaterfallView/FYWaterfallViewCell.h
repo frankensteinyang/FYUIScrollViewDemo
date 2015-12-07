11267e43c0fec1c90bbf447fa09c488ebca4c560
iHannibal <fredisstillalmighty@gmail.com>
2015-12-07 23:53:33 +0800

    瀑布流完善。

---
+++ FYUIScrollViewDemo/FYUIScrollViewDemo/Views/FYWaterfallView/FYWaterfallViewCell.h
@@ -0,0 +1,13 @@
+//
+//  FYWaterfallViewCell.h
+//  FYUIScrollViewDemo
+//
+//  Created by Frankenstein Yang on 12/7/15.
+//  Copyright © 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FYWaterfallViewCell : UIView
+
+@end
---
+++ FYUIScrollViewDemo/FYUIScrollViewDemo/Views/FYWaterfallView/FYWaterfallViewCell.m
@@ -0,0 +1,21 @@
+//
+//  FYWaterfallViewCell.m
+//  FYUIScrollViewDemo
+//
+//  Created by Frankenstein Yang on 12/7/15.
+//  Copyright © 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import "FYWaterfallViewCell.h"
+
+@implementation FYWaterfallViewCell
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
