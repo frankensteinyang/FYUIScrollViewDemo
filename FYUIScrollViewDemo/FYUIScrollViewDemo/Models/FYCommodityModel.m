492b79aa704a713b8784de37d628100ba8a5c351
yangjinzhong <fredisstillalmighty@gmail.com>
2015-12-12 23:37:26 +0800

    launch pictures

---
+++ FYUIScrollViewDemo/FYUIScrollViewDemo/Models/FYCommodityModel.m
@@ -0,0 +1,13 @@
+//
+//  FYCommodityModel.m
+//  FYUIScrollViewDemo
+//
+//  Created by Frankenstein Yang on 12/11/15.
+//  Copyright © 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import "FYCommodityModel.h"
+
+@implementation FYCommodityModel
+
+@end
