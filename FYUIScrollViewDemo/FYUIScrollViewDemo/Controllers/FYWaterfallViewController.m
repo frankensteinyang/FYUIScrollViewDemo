41039090d43a1f3185657080c475e41f3479dd4c
yangjinzhong <fredisstillalmighty@gmail.com>
2015-12-13 23:57:51 +0800

    瀑布流代码警告修复。

--- FYUIScrollViewDemo/FYUIScrollViewDemo/Controllers/FYWaterfallViewController.m
+++ FYUIScrollViewDemo/FYUIScrollViewDemo/Controllers/FYWaterfallViewController.m
@@ -7,7 +7,31 @@
 //
 
 #import "FYWaterfallViewController.h"
+#import "FYWaterfallView.h"
+
+@interface FYWaterfallViewController () <FYWaterfallViewDataSource, FYWaterfallViewDelegate>
+
+@end
 
 @implementation FYWaterfallViewController
+
+- (void)viewDidLoad {
+    
+    [super viewDidLoad];
+    FYWaterfallView *waterfallView = [[FYWaterfallView alloc] init];
+    waterfallView.frame = self.view.bounds;
+    waterfallView.dataSource = self;
+    waterfallView.delegate = self;
+    [self.view addSubview:waterfallView];
+}
+
+- (FYWaterfallViewCell *)waterfallView:(FYWaterfallView *)waterfallView cellAtIndex:(NSUInteger)index {
+    
+    return nil;
+}
+
+- (NSUInteger)numberOfCellsInWaterfallView:(FYWaterfallView *)waterfallView {
+    return 2;
+}
 
 @end
