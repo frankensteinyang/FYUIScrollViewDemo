1f42a106ea786606e012c2a4182fed02186359a8
iHannibal <fredisstillalmighty@gmail.com>
2015-12-22 23:52:43 +0800

    九宫格初构。

--- FYUIScrollViewDemo/FYUIScrollViewDemo/Views/FYGridView/FYGridView.h
+++ FYUIScrollViewDemo/FYUIScrollViewDemo/Views/FYGridView/FYGridView.h
@@ -8,6 +8,34 @@
 
 #import <UIKit/UIKit.h>
 
+typedef enum {
+    FYGridViewStylePush = 0,
+    FYGridViewStyleSwap
+} FYGridViewStyle;
+
+@protocol FYGridViewDataSource;
+@protocol FYGridViewActionDelegate;
+@protocol FYGridViewSortingDelegate;
+@protocol FYGridViewTransformationDelegate;
+@protocol FYGridViewLayoutStrategy;
+
 @interface FYGridView : UIView
+
+@property (nonatomic, weak) NSObject <FYGridViewDataSource> *dataSource;
+@property (nonatomic, weak) NSObject <FYGridViewActionDelegate> *actionDelegate;
+@property (nonatomic, weak) NSObject <FYGridViewSortingDelegate> *sortingDelegate;
+@property (nonatomic, weak) NSObject <FYGridViewTransformationDelegate> *transformDelegate;
+@property (nonatomic, strong) id <FYGridViewLayoutStrategy> layoutStrategy;
+@property (nonatomic, getter=isEditing) BOOL editing;
+@property (nonatomic, weak) UIView *mainSuperView;
+@property (nonatomic) FYGridViewStyle style;
+@property (nonatomic) NSInteger itemSpacing;
+@property (nonatomic) NSInteger itemHSpacing;
+@property (nonatomic) BOOL centerGrid;
+@property (nonatomic) UIEdgeInsets minimumPressDuration;
+@property (nonatomic) BOOL showFullSizeViewWithAlphaWhenTransforming;
+@property (nonatomic) BOOL showsVerticalScrollIndicator;
+@property (nonatomic) BOOL showsHorizontalScrollIndicator;
+@property (nonatomic, readonly) UIScrollView *scrollView;
 
 @end
---
+++ FYUIScrollViewDemo/FYUIScrollViewDemo/Views/FYGridView/FYGridViewCell.m
@@ -0,0 +1,21 @@
+//
+//  FYGridViewCell.m
+//  FYUIScrollViewDemo
+//
+//  Created by Frankenstein Yang on 12/22/15.
+//  Copyright © 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import "FYGridViewCell.h"
+
+@implementation FYGridViewCell
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
