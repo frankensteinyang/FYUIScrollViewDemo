b3f09f92709bec9c6cd13dd306fe8385d8e03f3f
yangjinzhong <fredisstillalmighty@gmail.com>
2015-12-26 23:45:14 +0800

    今天体检完后，感想颇深，做此页面记录病史。

---
+++ FYUIScrollViewDemo/FYUIScrollViewDemo/Controllers/FYMedicalRecordViewController.m
@@ -0,0 +1,16 @@
+//
+//  FYMedicalRecordViewController.m
+//  FYUIScrollViewDemo
+//
+//  Created by Frankenstein Yang on 12/26/15.
+//  Copyright © 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import "FYMedicalRecordViewController.h"
+
+@implementation FYMedicalRecordViewController
+
+// 身体状况记录，对自己负责
+
+
+@end
